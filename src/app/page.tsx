96c7ce620a0da4f0fcfc0bce4e1de10128ce2932
Patricia Eliseu <patyeliseu4@gmail.com>
2024-08-25 18:07:22 -0300

    alteraçoes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+
 import Header from "./components/header/header";
 import {Experience} from "./components/experience/experience";
 import {Info} from "./components/information/information";
@@ -8,7 +8,7 @@ import { SocialBtns } from "./components/social-btns/social-btns";
 export default function Home() {
   return (
     <main className="container">
-     <Header/>
+      <Header/>
       <Experience/>
       <Info/>
      
@@ -16,7 +16,7 @@ export default function Home() {
         <SocialBtns/>
         <a className="btn-primary" href="zap: (19) 98234-1686">
           Entre em contato
-          </a>
+        </a>
       </div>
            
         
