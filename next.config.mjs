ca448f8d3798fcbf6410454a925cb90643909f2f
Patricia Eliseu <patyeliseu4@gmail.com>
2024-08-28 17:47:06 -0300

    add config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+  basePath: '/patriciaeliseu.github.io/',
+  assetPrefix: '/patriciaeliseu.github.io/'
+};
 
 export default nextConfig;
