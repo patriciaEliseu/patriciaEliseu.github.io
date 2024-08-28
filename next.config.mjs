b941bf7672b3cabf43d2fbfcf2a66306aa21f682
Patricia Eliseu <patyeliseu4@gmail.com>
2024-08-28 17:54:22 -0300

    pasta out

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-  basePath: '/patriciaeliseu.github.io/',
+  basePath: '/patriciaeliseu.github.io',
   assetPrefix: '/patriciaeliseu.github.io/'
 };
 
