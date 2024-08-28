3463d33a71fbcf53a6feacb46202f1a7b6e08894
Patricia Eliseu <patyeliseu4@gmail.com>
2024-08-28 19:09:17 -0300

    deletando pasta out e inserindo images no next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,10 @@
 const nextConfig = {
     output: 'export',
   basePath: '/patriciaeliseu.github.io',
-  assetPrefix: '/patriciaeliseu.github.io/'
+  assetPrefix: '/patriciaeliseu.github.io/',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
